1236a2c1a27e702abe39f8a7aee001a07f5b07c5
Jonah Wallerstein <jwallers@gmail.com>
2014-03-22 14:52:54 -0400

    removed john haravard background

--- src/bitcom.backgrounds.js
+++ src/bitcom.backgrounds.js
@@ -11,7 +11,7 @@ bitcom.backgrounds = [];
 
 
     bitcom.backgrounds[1] = newBackground('backgrounds/Dorian.png',[246,488,736,524 ],[29,332,219,521], [1072, 33,1261,221])
-    bitcom.backgrounds[2] = newBackground('backgrounds/JohnHarvardBitcoin.jpg',[246,488,736,524 ],[29,332,219,521], [1072, 33,1261,221])
+//    bitcom.backgrounds[2] = newBackground('backgrounds/JohnHarvardBitcoin.jpg',[246,488,736,524 ],[29,332,219,521], [1072, 33,1261,221])
 
 })();
 
