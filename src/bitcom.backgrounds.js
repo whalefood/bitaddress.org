eae92fe8e7d69ad40cdafbe94747ea1fbdb5ade9
Jonah Wallerstein <jwallers@gmail.com>
2014-03-26 11:28:50 -0400

    Added some more background options

--- src/bitcom.backgrounds.js
+++ src/bitcom.backgrounds.js
@@ -11,7 +11,9 @@ bitcom.backgrounds = [];
 
 
     bitcom.backgrounds[1] = newBackground('backgrounds/Dorian.png',[246,488,736,524 ],[29,332,219,521], [1072, 33,1261,221])
-//    bitcom.backgrounds[2] = newBackground('backgrounds/JohnHarvardBitcoin.jpg',[246,488,736,524 ],[29,332,219,521], [1072, 33,1261,221])
+    bitcom.backgrounds[2] = newBackground('backgrounds/Blue.png',[246,488,736,524 ],[29,332,219,521], [1072, 33,1261,221])
+    bitcom.backgrounds[3] = newBackground('backgrounds/Orange.png',[246,488,736,524 ],[29,332,219,521], [1072, 33,1261,221])
+    bitcom.backgrounds[4] = newBackground('backgrounds/Green.png',[246,488,736,524 ],[29,332,219,521], [1072, 33,1261,221])
 
 })();
 
